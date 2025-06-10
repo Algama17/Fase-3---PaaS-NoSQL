fa0dbc6ca771ddcca4b746b392264809ed8ae74e
Algama17 <albertogarciamallada@gmail.com>
2025-06-10 14:16:17 +0200

    clean code

--- src/main/java/com/example/demo/AwsProperties.java
+++ src/main/java/com/example/demo/AwsProperties.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 public class AwsProperties {
 
     private String region;
-    private String dynamodbEndpoint; // opcional, solo si usas DynamoDB local
+    private String dynamodbEndpoint;
 
     public String getRegion() {
         return region;
